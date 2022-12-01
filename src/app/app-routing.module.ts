5bff3b65171291ca5e44d66a7ff3de53dca49818
LucasEG1 <legyori@gmail.com>
2022-12-01 14:26:21 +0100

    Teachers array + [TODO] teachers plist component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeachersPlistComponent } from './component/routed/teachers-plist/teachers-plist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', component: }
+  {path: 'teachers', component: TeachersPlistComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
